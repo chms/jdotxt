037e9cb3ddd9ea486a04a8cd3070962afae0b288
Christian M. Schmid <chms@gmx.net>
2014-06-28 16:27:40 +0200

    Version +1

--- src/com/chschmid/jdotxt/Jdotxt.java
+++ src/com/chschmid/jdotxt/Jdotxt.java
@@ -37,7 +37,7 @@ import com.todotxt.todotxttouch.task.TaskBag;
 import com.todotxt.todotxttouch.task.TaskBagFactory;
 
 public class Jdotxt {
-	public static final String VERSION = "0.4.3";
+	public static final String VERSION = "0.4.4";
 	public static final String APPID   = "chschmid.jdotxt";
 	
 	public static final int AUTOSAVE_DELAY = 3000;
